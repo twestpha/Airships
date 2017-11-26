24a42c863820b70463577a7b46049fde426fae08
Trevor Westphal <trevorawestphal@gmail.com>
2017-11-26 01:10:13 -0800

    Elevator working completely

--- Assets/Scripts/ElevatorInteract.cs
+++ Assets/Scripts/ElevatorInteract.cs
@@ -19,18 +19,29 @@ public class ElevatorInteract : MonoBehaviour {
     private Vector3 pistonRaised;
     private Vector3 pistonLowered;
 
+    private GameObject player;
+
+    public GameObject controls;
+    public Material idleTexture;
+    public Material activeTexture;
+
     void Start(){
         raiseTimer = new Timer(2.0f);
         waitTimer = new Timer(2.0f);
 
         pistonRaised = new Vector3(0.0f, 0.0f, 1.0f);
         pistonLowered = new Vector3(0.0f, 0.0f, -1.0f);
+
+        player = GameObject.FindWithTag("Player");
     }
 
     void FixedUpdate(){
         if(state == ElevatorState.Idle){
-            state = ElevatorState.Raising;
-            raiseTimer.Start();
+            if((transform.position - player.transform.position).magnitude < 1.5f && (Input.GetKeyDown("e") || Input.GetKeyDown("q"))){
+                state = ElevatorState.Raising;
+                controls.GetComponent<MeshRenderer>().material = activeTexture;
+                raiseTimer.Start();
+            }
         } else if(state == ElevatorState.Raising){
             pistonDriver.transform.localPosition = Vector3.Lerp(pistonLowered, pistonRaised, raiseTimer.Parameterized());
 
@@ -48,6 +59,7 @@ public class ElevatorInteract : MonoBehaviour {
 
             if(raiseTimer.Finished()){
                 state = ElevatorState.Idle;
+                controls.GetComponent<MeshRenderer>().material = idleTexture;
             }
         }
     }
